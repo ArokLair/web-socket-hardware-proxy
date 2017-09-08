3128247295a231cdbd124d4e6aa2bb4fe37dede9
Emily Ulloa <Emily Ulloa@192.168.1.10>
2017-09-07 23:49:21 -0500

    Init Commit

--- client-hw-proxy/src/ec/umbral/standardpro/agent/AgentClient.java
+++ client-hw-proxy/src/ec/umbral/standardpro/agent/AgentClient.java
@@ -3,7 +3,7 @@ package ec.umbral.standardpro.agent;
 public class AgentClient {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Initial");
 
 	}
 
